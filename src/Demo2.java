bf00e482387214b258f27814a6e8a08903a0af05
erdem <aerdemkamis@gmail.com>
2022-12-19 08:45:34 +0300

    Selenium Demo

--- src/Demo2.java
+++ src/Demo2.java
@@ -2,9 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class Demo1 {
+public class Demo2 {
     public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\ASUS\\Downloads\\chromedriver.exe");
@@ -12,16 +11,18 @@ public class Demo1 {
         driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx?ReturnUrl=%2fsamples%2ftestcomplete12%2fweborders%2fProcess.aspx");
 
         WebElement we = driver.findElement(By.id("ctl00_MainContent_username"));
-        we.sendKeys("Tester");
+        we.sendKeys("Teste");
         Thread.sleep(3000);
 
-        driver.findElement(By.name("ctl00$MainContent$password")).sendKeys("test");
+        driver.findElement(By.name("ctl00$MainContent$password")).sendKeys("tes");
         Thread.sleep(3000);
 
         driver.findElement(By.className("button")).click();
         Thread.sleep(3000);
 
-        driver.quit();
+        System.out.println(driver.findElement(By.cssSelector("#ctl00_MainContent_status")).getText());
+
+        //driver.quit();
 
 
     }
